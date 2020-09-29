acf64f6f8d900a0fd2e1236f1137de563996451d
xerxes <540813336@qq.com>
2020-09-29 11:15:49 +0800

    add new

--- src/main/java/com/test/git/HelloWorld.java
+++ src/main/java/com/test/git/HelloWorld.java
@@ -4,4 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public static void add() {
+        int a = 1 + 1;
+    }
 }
