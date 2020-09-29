aceb86d36da3e883513459ad7e284ec6cf04e8e9
xerxes <540813336@qq.com>
2020-09-29 11:23:59 +0800

    main change update

--- src/main/java/com/test/git/HelloWorld.java
+++ src/main/java/com/test/git/HelloWorld.java
@@ -3,5 +3,9 @@ package com.test.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hi wo geng xin la ");
+    }
+    public int abab(){
+        return 2;
     }
 }
