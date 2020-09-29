faf5447dce6df21b5a8c792323dd375768681cc5
xerxes <540813336@qq.com>
2020-09-29 11:27:44 +0800

    add new 2

--- src/main/java/com/test/git/HelloWorld.java
+++ src/main/java/com/test/git/HelloWorld.java
@@ -4,8 +4,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
+        System.out.println("7");
+        System.out.println("6");
+        System.out.println("5");
     }
 
     public static void add() {
