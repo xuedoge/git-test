d30b61435bd704b7f35cca20be4862fde550a572
xerxes <540813336@qq.com>
2020-09-29 10:46:24 +0800

    main change

--- src/main/java/com/test/git/HelloWorld.java
+++ src/main/java/com/test/git/HelloWorld.java
@@ -2,7 +2,10 @@ package com.test.git;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
     }
 
     public static void add() {
